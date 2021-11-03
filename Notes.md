6b1d1a220ceb75ce818a31e470defa18f3606675
Douglas Bates <dmbates@gmail.com>
2021-11-03 18:13:26 -0500

    Updates on subjtbl and trials.arrow

--- Notes.md
+++ Notes.md
@@ -17,7 +17,7 @@
     + some care is needed because the second header has an invalid DOB and the first header has 234 years of education.  Changed the first header to 2 years of ed., deleted the second.
 - Duplicate demographic blocks at the end of 404DATA.LDT, Data3034.LDT, 436DATA.LDT, 520DATA.LDT, Data1016.LDT, Data1634.LDT, Data1988.LDT
     + recommend using the last one, because we assume errors were corrected
-- 426DATA.LDT, Data1004.LDT, Data1009.LDT, Data1018.LDT, Data2042.LDT, Data2446.LDT are missing all the information in the demographic block.  Change the date of the MEQ test to 01-01-0000 so that it can be parsed as a Date.
+- 426DATA.LDT, Data1004.LDT, Data1009.LDT, Data1018.LDT, Data2042.LDT, Data2446.LDT are missing all the information in the demographic block. Add missingstring entry to CSV.File call.
 - 620DATA.LDT is missing most of the information in the demographic block.   Change the date of the MEQ test to 01-01-0000 so that it can be parsed as a Date.
 - Data1988.LDT is corrupt at sequence number 1744.  Sequence number 1745 can be salvaged by inserting a newline but probably not 1744 (response time is incomplete).
 - Data1988.LDT is missing the header for the second session.
--- scripts/ldt_extract.jl
+++ scripts/ldt_extract.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames
+using Arrow, CSV, DataFrames, Dates, PooledArrays
 
 const DATADIR = "./ldt_raw";
 
@@ -12,11 +12,11 @@ const SKIPLIST = [  # list of redundant or questionable files
 ];
 
 """
-    checktbuf(buf::IOBuffer)
+    checktbuf!(buf::IOBuffer)
 
 Return a DataFrame of the contents of `buf` assuming it contains trial information
 """
-function checktbuf(buf)
+function checktbuf!(buf)
     return(
         DataFrame(
             CSV.File(
@@ -28,6 +28,15 @@ function checktbuf(buf)
         )
     )
 end
+
+"""
+    checkhbuf!(buf::IOBuffer, types::Vector{DataType}, missingstring="")
+
+Return a DataFrame from CSV.File on `buf` with optional arguments `types` and `missingstring`
+
+`buf` is empty upon return
+"""
+checkhbuf!(buf,types,missingstring="") = DataFrame(CSV.File(take!(buf);types,missingstring))
 
 """
     skipblanks(strm)
@@ -43,9 +52,9 @@ function skipblanks(strm)
 end
 
 """
-    parse_ldt_file(fnm)
+    parse_ldt_file(fnm, dir=DATADIR)
 
-Return a NamedTuple of `fnm` and 6 DataFrames from LDT file with path `fnm`
+Return a NamedTuple of `fnm` and 6 DataFrames from LDT file with name `fnm` in directory `dir`
 """
 function parse_ldt_file(fnm, dir=DATADIR)
     @show fnm
@@ -67,40 +76,27 @@ function parse_ldt_file(fnm, dir=DATADIR)
     while true     # loop over lines in file
         if startswith(ln, univhdr)   # header for session 2
             write(hdrbuf, readline(strm; keep))   # second line of univ data
-            univ = DataFrame(CSV.File(
-                take!(hdrbuf);
-                types=[Int8, String, String, Int16, String, Int16],
-                ),
-            )
-            sess1 = checktbuf(trialbuf)
+            univ = checkhbuf!(hdrbuf, [Int8, String, String, Int16, String, Int8])
+            sess1 = checktbuf!(trialbuf)
         elseif startswith(ln, subjhdr)
-            sess2 = checktbuf(trialbuf)
+            sess2 = checktbuf!(trialbuf)
             write(hdrbuf, ln)
             write(hdrbuf, readline(strm; keep))
-            subj = DataFrame(CSV.File(
-                take!(hdrbuf);
-                types=[Int16, String, String, Float32, String, String],
-                )
+            subj = checkhbuf!(
+                hdrbuf,
+                [Int16, String, String, Float32, String, String],
+                ["","00:00:00","00-00-0000", "x"],
             )
             ln = skipblanks(strm)
             startswith(ln, ncorhdr) || throw(ArgumentError("Expected $ncorhdr, got $ln"))
             write(hdrbuf, ln)
             write(hdrbuf, readline(strm; keep))
-            ncor = DataFrame(CSV.File(
-                take!(hdrbuf);
-                types=[Int8, Int8, Float32, Int8, Float32],
-                missingstring="999",
-                ),
-            )
+            ncor = checkhbuf!(hdrbuf, [Int8, Int8, Float32, Int8, Float32], "999")
             ln = skipblanks(strm)
             startswith(ln, hlthhdr) || throw(ArgumentError("Expected $hlthhdr, got $ln"))
             write(hdrbuf, ln)
             write(hdrbuf, readline(strm; keep))
-            hlth = DataFrame(CSV.File(
-                take!(hdrbuf);
-                types=[Int8, Int8, Int8, Int8, String],
-                ),
-            )
+            hlth = checkhbuf!(hdrbuf, [Int8, Int8, Int8, Int8, String], ["","Unknown","-1"])
             break
         else
             write(trialbuf, ln)
@@ -114,11 +110,95 @@ end
 dfs =
 [parse_ldt_file(nm) for nm in filter(∉(SKIPLIST), filter(endswith(r"LDT"i), readdir(DATADIR)))];
 
+"""
+    checksubj(nt::NamedTuple)
+
+Return the subject number after checking for consistency in the three different places it is stored,
+"""
 function checksubj(nt)
     subjno = only(unique(nt.univ.Subject))
     return only(nt.subj.Subject) == subjno ? subjno : nothing
 end
 
+"""
+    getDOB(nt::NamedTuple)
+
+Return the unique element of `nt.univ.DOB` or both elements separated by `'|'`
+"""
 getDOB(nt) = join(unique(nt.univ.DOB), '|')
 
+"""
+    getEduc(nt::NamedTuple)
+
+Return the maximum value of `nt.univ.Education`.
+
+The value was supposed to be years of education but some are recorded as years of university,
+which is why the maximum is returned.
+"""
 getEduc(nt) = maximum(nt.univ.Education)
+
+"""
+    parseDate(dfrow::DataFrameRow)
+
+Return a `DateTime` from parsing the `Date` and `Time` fields where the date is "mm-dd-yyyy"
+"""
+function parseDate(dfrow)
+    return passmissing(DateTime)(
+        passmissing(string)(dfrow.Date, 'T', dfrow.Time),
+        dateformat"mm-dd-yyyyTH:M:S",
+        )
+end 
+
+subjtbl = sort!(
+    DataFrame(
+        [
+            (
+                subj = checksubj(nt),
+                univ = only(unique(nt.univ.Univ)),
+                sex = only(nt.subj.Gender),
+                MEQ = only(nt.subj.MEQ),
+                vision = only(nt.hlth.vision),
+                hearing = only(nt.hlth.hearing),
+                educatn = getEduc(nt),
+                ncorrct = only(nt.ncor.numCorrect),
+                rawscor = only(nt.ncor.rawScore),
+                vocabAge = only(nt.ncor.vocabAge),
+                shipTime = only(nt.ncor.shipTime),
+                readTime = only(nt.ncor.readTime),
+                preshlth = only(nt.hlth.presHealth),
+                pasthlth = only(nt.hlth.pastHealth),
+                S1start = parseDate(first(nt.univ)),
+                S2start = parseDate(last(nt.univ)),
+                MEQstrt = parseDate(only(nt.subj)),
+                filename = nt.fnm,
+                frstLang = only(nt.hlth.firstLang),
+            ) for nt in dfs
+        ]
+    ),
+    :subj
+)
+
+subjtbl.sex = PooledArray(subjtbl.sex; compress=true, signed=true);
+subjtbl.frstLang = PooledArray(subjtbl.frstLang; compress=true, signed=true);
+subjtbl.rawscor = [(ismissing(x) || x < 3) ? missing : x for x in subjtbl.rawscor];
+subjtbl.ncorrct = [(ismissing(x) || x < 3) ? missing : x for x in subjtbl.ncorrct];
+subjtbl.vocabAge = [(ismissing(x) || x < 3 || x > 25) ? missing : x for x in subjtbl.vocabAge];
+subjtbl.readTime = [(ismissing(x) || x < 0) ? missing : x for x in subjtbl.readTime];
+subjtbl.MEQ = [(ismissing(x) || x < 3) ? missing : x for x in subjtbl.MEQ];
+Arrow.write("./arrow/subjtbl.arrow", subjtbl; compress=:zstd)
+
+"""
+    freqtbl(nm::Symbol, df::DataFrame)
+
+Return a DataFrame with the sorted unique values of df.nm and their frequency
+"""
+freqtbl(nm, df) = sort(combine(groupby(df, nm), nrow => :n), nm)
+
+function trtbl(nt)
+    subj, sess1, sess2 = checksubj(nt), nt.sess1, nt.sess2
+    df = select(append!(copy(sess1), sess2), 1 => (x -> subj) => :subj, :seq, [:itemgrp, :wrd] => ((x,y) -> 2 * x - y) => :itemno, :acc, :rt)
+    df.s2 = append!(repeat([false], nrow(sess1)), repeat([true], nrow(sess2)))
+    return df
+end 
+    
+
